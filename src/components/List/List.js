df63472a978f1efd08ca17fbd6176b356658aa23
Benjamin Marguin <benjamin.marguin@makina-corpus.com>
2018-02-19 17:42:53 +0100

    Add a new list item

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -9,6 +9,7 @@ class List extends Component {
         <li>Perspiciatis sequi officiis aXt atque neque eveniet deserunt, necessitatibus ducimus</li>
         <li>Quidem delectus veritatis tenetur culpa iusto ab minus! Ut arChitecto consequatur incidunt cupiditate</li>
         <li>Architecto nemo placeat sed animi ad atque laborum mollitia non ea suscipit</li>
+        <li>Perferendis voluptatem unde maxime eaque fuga consequatur inventore est, dolores.</li>
       </ul>
     );
   }
