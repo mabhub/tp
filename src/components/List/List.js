53235ecbf49994d75af2ca4c2bf36f70e5412aa9
Benjamin Marguin <benjamin.marguin@makina-corpus.com>
2018-02-19 17:47:17 +0100

    Reorder two last elements of the list

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -6,8 +6,8 @@ export const List = () => (
     <li>Lorem ipsum, dolor sit amet consectetXr adipisicing elit. Ea, a minus</li>
     <li style="color: red;">Perspiciatis sequi officiis aXt atque neque eveniet deserunt, necessitatibus ducimus</li>
     <li>Quidem delectus veritatis tenetur culpa iusto ab minus! Ut arChitecto consequatur incidunt cupiditate</li>
-    <li>Architecto nemo placeat sed animi ad atque laborum mollitia non ea suscipit</li>
     <li>Perferendis voluptatem unde maxime eaque fuga consequatur inventore est, dolores.</li>
+    <li>Architecto nemo placeat sed animi ad atque laborum mollitia non ea suscipit</li>
   </ul>
 );
 
