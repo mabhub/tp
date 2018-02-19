a4f89e500b579a6fec0c636c4cc29b9ca37b6adf
Benjamin Marguin <benjamin.marguin@makina-corpus.com>
2018-02-19 17:42:08 +0100

    Fix another typo in text

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -5,8 +5,8 @@ class List extends Component {
     return (
       <ul>
         <li>Lorem ipsum, dolor sit amet consectetXr adipisicing elit. Ea, a minus</li>
-        <li>Perspiciatis sequi officiis aut atque neque eveniet deserunt, necessitatibus ducimus</li>
-        <li>Quidem delectus veritatis tenetur culpa iusto ab minus! Ut architecto consequatur incidunt cupiditate</li>
+        <li>Perspiciatis sequi officiis aXt atque neque eveniet deserunt, necessitatibus ducimus</li>
+        <li>Quidem delectus veritatis tenetur culpa iusto ab minus! Ut arChitecto consequatur incidunt cupiditate</li>
         <li>Non a ab dolorum obcaecati Xempore, harum hic aspernatur corrupti quaerat, laudantium</li>
         <li>Architecto nemo placeat sed animi ad atque laborum mollitia non ea suscipit</li>
       </ul>
