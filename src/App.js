cb45080e45c0c4ecf058c7f090e92f789d36b246
Benjamin Marguin <benjamin.marguin@makina-corpus.com>
2018-02-19 17:23:13 +0100

    Add CustomTable component to main App

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import CustomTable from './components/CustomTable/CustomTable';
 import logo from './logo.svg';
 import './App.css';
 
@@ -10,6 +11,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
+        <CustomTable />
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
