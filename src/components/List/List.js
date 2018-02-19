880ab22433881914880dd26d52d5e883ae26579a
Benjamin Marguin <benjamin.marguin@makina-corpus.com>
2018-02-19 17:42:19 +0100

    Change list items order

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -4,10 +4,10 @@ class List extends Component {
   render() {
     return (
       <ul>
+        <li>Non a ab dolorum obcaecati Xempore, harum hic aspernatur corrupti quaerat, laudantium</li>
         <li>Lorem ipsum, dolor sit amet consectetXr adipisicing elit. Ea, a minus</li>
         <li>Perspiciatis sequi officiis aXt atque neque eveniet deserunt, necessitatibus ducimus</li>
         <li>Quidem delectus veritatis tenetur culpa iusto ab minus! Ut arChitecto consequatur incidunt cupiditate</li>
-        <li>Non a ab dolorum obcaecati Xempore, harum hic aspernatur corrupti quaerat, laudantium</li>
         <li>Architecto nemo placeat sed animi ad atque laborum mollitia non ea suscipit</li>
       </ul>
     );
