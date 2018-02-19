f5b3a9c3694e51b044c341d4120211db34348c57
Benjamin Marguin <benjamin.marguin@makina-corpus.com>
2018-02-19 17:17:06 +0100

    Create `<List />` component

---
+++ src/components/List/List.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class List extends Component {
+  render() {
+    return (
+      <ul>
+        <li>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ea, a minus</li>
+        <li>Perspiciatis sequi officiis aut atque neque eveniet deserunt, necessitatibus ducimus</li>
+        <li>Quidem delectus veritatis tenetur culpa iusto ab minus! Ut architecto consequatur incidunt cupiditate</li>
+        <li>Non a ab dolorum obcaecati tempore, harum hic aspernatur corrupti quaerat, laudantium</li>
+        <li>Architecto nemo placeat sed animi ad atque laborum mollitia non ea suscipit</li>
+      </ul>
+    );
+  }
+}
+
+export default List;
