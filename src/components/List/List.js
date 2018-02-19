f987e4d783558f60c47110dbd9cc0ba1e78e81a8
Benjamin Marguin <benjamin.marguin@makina-corpus.com>
2018-02-19 17:46:20 +0100

    Color third List line in red

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -4,7 +4,7 @@ export const List = () => (
   <ul>
     <li>Non a ab dolorum obcaecati Xempore, harum hic aspernatur corrupti quaerat, laudantium</li>
     <li>Lorem ipsum, dolor sit amet consectetXr adipisicing elit. Ea, a minus</li>
-    <li>Perspiciatis sequi officiis aXt atque neque eveniet deserunt, necessitatibus ducimus</li>
+    <li style="color: red;">Perspiciatis sequi officiis aXt atque neque eveniet deserunt, necessitatibus ducimus</li>
     <li>Quidem delectus veritatis tenetur culpa iusto ab minus! Ut arChitecto consequatur incidunt cupiditate</li>
     <li>Architecto nemo placeat sed animi ad atque laborum mollitia non ea suscipit</li>
     <li>Perferendis voluptatem unde maxime eaque fuga consequatur inventore est, dolores.</li>
