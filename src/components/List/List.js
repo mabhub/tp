bc1baca7dceef568eb2385ebebc2b116630ce27f
Benjamin Marguin <benjamin.marguin@makina-corpus.com>
2018-02-19 17:49:25 +0100

    Turn third line in blue instead of red

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -4,7 +4,7 @@ export const List = () => (
   <ul>
     <li>Non a ab dolorum Xbcaecati XempXre, harum hic aspernatur cXrrupti quaerat, laudantium</li>
     <li>LXrem ipsum, dXlXr sit amet cXnsectetXr adipisicing elit. Ea, a minus</li>
-    <li style="color: red;">Perspiciatis sequi Xfficiis aXt atque neque eveniet deserunt, necessitatibus ducimus</li>
+    <li style="color: blue;">Perspiciatis sequi Xfficiis aXt atque neque eveniet deserunt, necessitatibus ducimus</li>
     <li>Quidem delectus veritatis tenetur culpa iustX ab minus! Ut arChitectX cXnsequatur incidunt cupiditate</li>
     <li>Perferendis vXluptatem unde maxime eaque fuga cXnsequatur inventXre est, dXlXres.</li>
     <li>ArchitectX nemX placeat sed animi ad atque labXrum mXllitia non ea suscipit</li>
