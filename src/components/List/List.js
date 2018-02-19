c505ace5e04a6a61131f584468d459599c55f231
Benjamin Marguin <benjamin.marguin@makina-corpus.com>
2018-02-19 17:48:49 +0100

    Replace O's by X's in list texts

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -2,12 +2,12 @@ import React, { Component } from 'react';
 
 export const List = () => (
   <ul>
-    <li>Non a ab dolorum obcaecati Xempore, harum hic aspernatur corrupti quaerat, laudantium</li>
-    <li>Lorem ipsum, dolor sit amet consectetXr adipisicing elit. Ea, a minus</li>
-    <li style="color: red;">Perspiciatis sequi officiis aXt atque neque eveniet deserunt, necessitatibus ducimus</li>
-    <li>Quidem delectus veritatis tenetur culpa iusto ab minus! Ut arChitecto consequatur incidunt cupiditate</li>
-    <li>Perferendis voluptatem unde maxime eaque fuga consequatur inventore est, dolores.</li>
-    <li>Architecto nemo placeat sed animi ad atque laborum mollitia non ea suscipit</li>
+    <li>Non a ab dolorum Xbcaecati XempXre, harum hic aspernatur cXrrupti quaerat, laudantium</li>
+    <li>LXrem ipsum, dXlXr sit amet cXnsectetXr adipisicing elit. Ea, a minus</li>
+    <li style="color: red;">Perspiciatis sequi Xfficiis aXt atque neque eveniet deserunt, necessitatibus ducimus</li>
+    <li>Quidem delectus veritatis tenetur culpa iustX ab minus! Ut arChitectX cXnsequatur incidunt cupiditate</li>
+    <li>Perferendis vXluptatem unde maxime eaque fuga cXnsequatur inventXre est, dXlXres.</li>
+    <li>ArchitectX nemX placeat sed animi ad atque labXrum mXllitia non ea suscipit</li>
   </ul>
 );
 
